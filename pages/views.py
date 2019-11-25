e51b9cc30da00e7a3f651557ff295f71812bc266
davit <davit@Davit-Ubuntu>
2019-11-25 19:08:31 +0400

    commit

--- pages/views.py
+++ pages/views.py
@@ -9,12 +9,12 @@ from django.contrib.auth import logout
 def mainView(request):
     User = f"სახელი: {request.user.first_name} გვარი:{request.user.last_name}"
     form = MessageForm()
-    messages = Message.objects.all()
+    messages = None
     if request.POST:
-        messages = Message.objects.all()
         form = MessageForm(request.POST)
         message = form['message'].value()
         Message(author=request.user.first_name, message=message).save()
+        messages = Message.objects.all()
 
 
     return render(request, 'main.html', {'messages':messages,'form':form,"userinfo":User})
