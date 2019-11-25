c72d3c64e6da82d8c9b17d5b4e69008049625692
davit <davit@Davit-Ubuntu>
2019-11-25 18:46:50 +0400

    commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from accounts.models import User
-from django.contrib.auth.forms import UserCreationForm
+from accounts.authentication.customforms import SignUpForm
 
 
-class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+class RegisterForm(SignUpForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control",}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control"}))
     age = forms.IntegerField(widget=forms.NumberInput(attrs={"class":"form-control"}))
+    password1 = forms.CharField(label="Password",widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    password2 = forms.CharField(label="Confirmation Password",widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
         fields = ('first_name','last_name','email','age','password1','password2')
-
 
 
